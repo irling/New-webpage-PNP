1a533fd4772625737c50659952b1d7266caa6472
irling <italodavidramosling@gmail.com>
2025-05-04 21:09:40 -0500

    PROYECTO FINALIZADO

--- js/index.js
+++ js/index.js
@@ -32,3 +32,46 @@ document.addEventListener('click',function () {
     });
   });
   
+  const images = document.querySelectorAll('.carousel-img');
+  const textContainer = document.getElementById('carousel-text');
+
+  const textos = [
+    `
+    <h1 class="text-9xl font-bold leading-tight">SERVICIOS <br />POLICIALES <br />DIGITALES</h1>
+    <p class="text-yellow-400 mt-4 text-5xl italic">Acortando tiempo y distancia</p>
+    <hr class="w-145 mt-5 font-bold text-amarillo-claro" />
+    <hr class="w-145 font-bold text-amarillo-claro" />
+    <p class="mt-2 text-3xl font-light">Accede a servicios policiales en línea de forma rápida, segura y disponible las 24 horas</p>
+    <button class="mt-6 bg-amarillo-claro text-black font-semibold py-2 px-4 rounded-xl cursor-pointer hover:bg-fondo hover:text-verde-oscuro">CONOCE MÁS AQUÍ</button>
+    `,
+    `
+    <h1 class="text-9xl font-bold leading-tight">Cuidamos<br /> tu mundo digital</h1>
+    <hr class="w-145 mt-5 font-bold text-amarillo-claro" />
+    <hr class="w-145 font-bold text-amarillo-claro" />
+    <p class="mt-2 text-3xl font-light">Consejos y herramientas para <br/> una navegación segura para toda la familia.</p>
+    <button class="mb-15 mt-6 bg-amarillo-claro text-black font-semibold py-2 px-4 rounded-xl cursor-pointer hover:bg-fondo hover:text-verde-oscuro">CONOCE MÁS AQUÍ</button>
+    `,
+    `
+    <h1 class="text-9xl font-bold leading-tight">Alerta de <br/> ciberamenaza!</h1>
+    <hr class="w-145 mt-5 font-bold text-amarillo-claro" />
+    <hr class="w-145 font-bold text-amarillo-claro" />
+    <p class="mt-2 text-2xl font-light">Conoce las estafas más recientes y cómo protegerlas</p>
+    <button class="mb-15  mt-6 bg-amarillo-claro text-black font-semibold py-2 px-4 rounded-xl cursor-pointer hover:bg-fondo hover:text-verde-oscuro">PROTÉGETE AHORA</button>
+    `
+  ];
+
+  let currentIndex = 0;
+
+  function showImage(index) {
+    images.forEach((img, i) => {
+      img.style.opacity = i === index ? 1 : 0;
+    });
+    textContainer.innerHTML = textos[index];
+    currentIndex = index;
+  }
+
+  // Auto-cambio opcional
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }, 7000); // cambia cada 7 segundos
